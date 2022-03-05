766a98f4f3127292c01bd2d1430d8b298defaed4
Aiden <aiden.p2908@gmail.com>
2022-03-05 19:00:01 +1300

    Add project files.

---
+++ cmsProject-web/Program.cs
@@ -0,0 +1,34 @@
+using cmsProject_web.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<CmsDbContext>(options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("codexDB")//comes from appsettings json that we set up
+));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
